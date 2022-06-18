2662e243f02578a8048b1b5dea341dc732a880a9
MATHEUS HENRIQUE SALDIAS PALACIOS PIETRO BIAS <biasimatheushenrique40@gmail.com>
2022-06-17 23:31:39 -0300

    Adiconando condicao de saque e saldo na conta corrente

--- JS Orientado a objetos/ByteBankClientes.js
+++ JS Orientado a objetos/ByteBankClientes.js
@@ -10,17 +10,34 @@ class contaCorente{
     saldo
 }
 
+// obejto com dados do cliente
 const cliente1 = new Cliente()
 cliente1.nome = "Matheus"
 cliente1.cpf = 12156879536
 
-
+// objeto com dados da conta do cliente
 const contaCorenteMatheus = new contaCorente()
 contaCorenteMatheus.agencia = 1001
 contaCorenteMatheus.saldo = 0
 
+//Fazendo deposito na conta
+contaCorenteMatheus.saldo = 100
+
+//Realizando saque
+let valorSacado = 50
+
+if (contaCorenteMatheus.saldo>valorSacado){
+    contaCorenteMatheus.saldo -= valorSacado
+    console.log(`Novo saldo em conta: R$ ${contaCorenteMatheus.saldo}`)
+    console.log(`Valor do saque: R$ ${valorSacado}.`)
+}else {
+    console.log(`Voce nao possui em conta o valor solicitado!\n Saldo em conta: R$ ${contaCorenteMatheus.saldo}`)
+}
+
+
+
 console.log(cliente1)
-console.log(contaCorenteMatheus.agencia)
+console.log(contaCorenteMatheus.saldo)
 
 
 
