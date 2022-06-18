73024dea560086046382370d567bf08e6bb358b0
MATHEUS HENRIQUE SALDIAS PALACIOS PIETRO BIAS <biasimatheushenrique40@gmail.com>
2022-06-17 23:16:47 -0300

    Criando classes com dados do cliente

--- JS Orientado a objetos/ByteBankClientes.js
+++ JS Orientado a objetos/ByteBankClientes.js
@@ -1,1 +1,30 @@
-  
+  // Criando classes de clientes para Byte bank.
+
+ class Cliente{
+    nome
+    cpf
+  }
+
+class contaCorente{
+    agencia
+    saldo
+}
+
+const cliente1 = new Cliente()
+cliente1.nome = "Matheus"
+cliente1.cpf = 12156879536
+
+
+const contaCorenteMatheus = new contaCorente()
+contaCorenteMatheus.agencia = 1001
+contaCorenteMatheus.saldo = 0
+
+console.log(cliente1)
+console.log(contaCorenteMatheus.agencia)
+
+
+
+
+
+
+
